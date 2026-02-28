14754a99ec418039437cd775a6a9d286743a33e0
ManfredasLamsargis <manfredas.lamsargis@proton.me>
2026-02-28 15:54:15 +0200

    feat: add docker installation script (does not have executable rights for safety)

---
+++ conf/docker_install.sh
@@ -0,0 +1,25 @@
+# Update packages
+sudo apt update
+
+# Install dependencies
+sudo apt install -y ca-certificates curl gnupg
+
+# Add Docker's GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add Docker repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Allow your user to run Docker without sudo
+# Required by applications like Whaler
+sudo usermod -aG docker $USER
+newgrp docker
